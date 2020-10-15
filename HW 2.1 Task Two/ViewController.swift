4f2671c29d9fdc25feb127b34cccc7536ab17259
arkadiyohotnikov <72878465+arkadiyohotnikov@users.noreply.github.com>
2020-10-15 05:11:35 +0300

    Final

--- HW 2.1 Task Two/ViewController.swift
+++ HW 2.1 Task Two/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redSignalView: UIView!
+    @IBOutlet weak var yellowSignalView: UIView!
+    @IBOutlet weak var greenSignailView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        redSignalView.layer.cornerRadius = 63
+        yellowSignalView.layer.cornerRadius = 63
+        greenSignailView.layer.cornerRadius = 63
+        startButton.layer.cornerRadius = 10
+        }
 
+    @IBAction func startButtonPressed () {
+       startButton.setTitle("NEXT", for: .normal)
+        redSignalView.alpha = 1
+             }                   }
+           
 
-}
-
+        
